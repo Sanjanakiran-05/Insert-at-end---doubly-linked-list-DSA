e603807aec3bd79d18de86b47b287017cbbc04d1
Sanjanakiran-05 <sanjanakirang@gmail.com>
2024-12-04 14:52:51 +0530

    Add files via upload

---
+++ DoublyLinkedList (1).java
@@ -0,0 +1,64 @@
+import java.util.*;
+class DoublyLinkedList
+{
+    Node head;
+    class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            prev=null;
+            next=null;
+        }
+    }
+    public void insert_at_end(int num) 
+    {
+        Node newNode = new Node(num);
+        if (head == null) 
+        {
+            head = newNode;
+        } 
+        else 
+        { 
+            Node temp=head;
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            newNode.prev=temp;
+        }
+    }
+    public void display() 
+    {
+        Node temp = head;
+        while (temp != null) 
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        DoublyLinkedList dll = new DoublyLinkedList();
+        System.out.println("Enter the items to insert:");
+        while (true) 
+        {
+            int num = s.nextInt();
+            if (num == -1) 
+            {
+                break;
+            }
+            dll.insert_at_end(num); 
+        }
+        System.out.println("Enter element to insert at end: ");
+        int n=s.nextInt();
+        dll.insert_at_end(n);
+        dll.display();
+    }
+}
